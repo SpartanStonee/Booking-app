ff5b3e250ce8bdc757d329f55ca65ebabc3cd8b9
SpartanStonee <yaron.froyen@gmail.com>
2023-06-06 14:39:18 +0200

    Add project files.

---
+++ Api/models/Hotel.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const HotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true
+    },
+    type: {
+        type: String,
+        required:true
+    }
+})
